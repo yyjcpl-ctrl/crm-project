763f9052de4f9eb1966116295048ea7c77656f91
KASTURI ARCHITECTS <acss@Acsss-MacBook-Air.local>
2026-02-17 19:51:57 +0530

    fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  redirect("/login");
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>CRM Home</h1>
+
+      <div style={{ marginTop: 20 }}>
+        <Link href="/dashboard">Go to Dashboard</Link>
+      </div>
+    </div>
+  );
 }
